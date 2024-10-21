b0b9d1217ac66537d1e001ce125cc3f5821b5d92
Mario Arturo Serrano Flores <mario.serrano@imssbienestar.gob.mx>
2024-10-21 14:16:48 -0700

    Agregar totales

--- src/reports/reporte-paises.ts
+++ src/reports/reporte-paises.ts
@@ -1,6 +1,6 @@
 import { TDocumentDefinitions } from "pdfmake/interfaces";
 import { headerSection } from "./sections/header.section";
-import { countries as Pais} from "@prisma/client";
+import { countries as Pais } from "@prisma/client";
 import { footerSection } from "./sections/footer.section";
 
 interface ReportOptions {
@@ -34,15 +34,55 @@ export const getReportePaises = (options: ReportOptions): TDocumentDefinitions =
                     body: [
                         ['ID', 'ISO2', 'ISO3', 'Name', 'Continent', 'Local Name'],
                         ...paises.map(pais => [
-                             pais.id.toString(),
-                             pais.iso2,
-                             pais.iso3,
+                            pais.id.toString(),
+                            pais.iso2,
+                            pais.iso3,
                             { text: `${pais.name}`, bold: true },
-                             pais.continent,
-                             pais.local_name])
+                            pais.continent,
+                            pais.local_name]),
+                        ['', '', '', '', '', ``],
+                        ['', '', '', '', 'Total países',
+                            {
+                                text: `${paises.length}`,
+                                bold: true
+                            },
+                        ],
                     ]
                 }
             },
+            // Tabla de totales
+            {
+                text: 'Totales',
+                style: {
+                    fontSize: 18,
+                    bold: true,
+                    margin: [0, 40, 0, 0]
+                }
+            },
+            {
+                layout: 'noBorders',
+                table: {
+                    headerRows: 1,
+                    widths: [50, 50, 50, '*', '*', '*'],
+                    body: [
+                        [
+                            {
+                                text: 'Total de paises',
+                                colSpan: 2,
+                                bold: true
+                            },
+                            {},
+                            {
+                                text: `${paises.length}`,
+                                bold: true
+                            },
+                            {},
+                            {},
+                            {},
+                        ]
+                    ],
+                },
+            }
         ],
     };
 
