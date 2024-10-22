6923d9c49795d493dcad05f2f8eae389562335fa
Mario Arturo Serrano Flores <mario.serrano@imssbienestar.gob.mx>
2024-10-22 12:49:53 -0700

    estilo en la misma linea

--- src/reports/order-by-id.report.ts
+++ src/reports/order-by-id.report.ts
@@ -1,5 +1,6 @@
 import { Content, StyleDictionary, TDocumentDefinitions } from "pdfmake/interfaces";
 import { DateFormatter } from "src/helpers";
+import { footerSection } from "./sections/footer.section";
 
 const logo: Content = {
     image: 'src/assets/imssb-banner.png',
@@ -14,6 +15,11 @@ const styles: StyleDictionary = {
         bold: true,
         margin: [0, 60, 0, 0],
     },
+    subHeader : {
+        fontSize: 16,
+        bold: true,
+        margin: [0, 10, 0, 5],
+    }
 }
 
 export const orderByIdReport = (): TDocumentDefinitions => {
@@ -21,6 +27,7 @@ export const orderByIdReport = (): TDocumentDefinitions => {
         styles: styles,
         header: logo,
         pageMargins: [40, 60, 40, 60],
+        footer: footerSection({ mostrarNumeroPagina: true }),
         content: [
             // Encabezado
             {
@@ -34,7 +41,10 @@ export const orderByIdReport = (): TDocumentDefinitions => {
                         text: 'Gustavo E Campa 54, \nGuadalupe Inn, Alvaro Obregon 01020, Ciudad de México\nRFC: SSI220901JS5\nhttps://www.imssbienestar.gob.mx/',
                     },
                     {
-                        text: `Documento No. #123456\nFecha: ${DateFormatter.getDDMMMMYYYY(new Date())}\nVencimiento: ${DateFormatter.getDDMMMMYYYY(new Date())}\n`,
+                        text: [
+                            { text: 'Documento No. #123456\n', bold: true },
+                            `Fecha: ${DateFormatter.getDDMMMMYYYY(new Date())}\nVencimiento: ${DateFormatter.getDDMMMMYYYY(new Date())}\n`
+                        ],
                         alignment: 'right',
                     }
                 ]
@@ -43,6 +53,14 @@ export const orderByIdReport = (): TDocumentDefinitions => {
                 qr: 'https://www.imssbienestar.gob.mx/',
                 fit: 75,
                 alignment: 'right'
+            },
+
+            // Direccion de almacén
+            {
+                text: [
+                    { text: 'Dirigido a\n', style: 'subHeader' },
+                    `Almacén Zona Mexicali\nCalle Ignacio Zaragoza S/N, Col. Ejido Puebla,\nC.P. 21620, Mexicali B.C.\n`
+                ]
             }
         ],
     }
